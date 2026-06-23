18cb8252197fd0ae9fdd8690005544bc9f4b9421
mfabri31 <fabriciomedina9531@gmail.com>
2026-06-23 00:30:55 -0300

    test: implement add-to-cart functionality

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -24,6 +24,7 @@ class TestShoppingCart:
         # webdriver.implicitly_wait(10)
         inventory_list = webdriver.find_element(By.CLASS_NAME, "inventory_list")
         assert inventory_list.is_displayed() == True
+  
 
         # sleep(10)
         webdriver.quit()
@@ -32,8 +33,18 @@ class TestShoppingCart:
     
     def test_should_add_item(self,setup:WebDriver):
         webdriver = setup
-        btn_add = webdriver.find_elements(By.CSS_SELECTOR, '[data-test^="add-to-cart"]')
-        assert  len(btn_add) > 0
+        buttons_add = webdriver.find_elements(By.CSS_SELECTOR, '[data-test^="add-to-cart"]')
+        assert  len(buttons_add) > 0
 
-        # btn_add.click()
-        # sleep(10)
+        first_product = buttons_add[0]
+        first_product.click()
+
+        webdriver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+
+        current_url = webdriver.current_url
+        assert "cart" in current_url
+      
+        products_added = webdriver.find_elements(By.CLASS_NAME,"cart_item")
+        products_in_cart = len(products_added)
+        assert products_in_cart == 1
+        
